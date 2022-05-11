0b8422c23c1c984288a305b5e7093dd349b2d4f3
jester69m <paschuknazar@ukr.net>
2022-05-11 12:35:07 +0300

    Edit produxt.class(Add Group name as parameter)

--- src/source/Product.java
+++ src/source/Product.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
  * */
 public class Product {
 
+    private String groupNameInProduct;
     private String productName;
     private String productDescription;
     private String producer;
@@ -16,17 +17,19 @@ public class Product {
 
     private ArrayList<Product> products = new ArrayList<>();
 
-    private Product(String productName, String productDescription, String producer, int productQuantityOnStock, double productPrice) {
+    private Product(String groupNameInProduct, String productName, String productDescription, String producer, int productQuantityOnStock, double productPrice) {
+        this.groupNameInProduct=groupNameInProduct;
         this.productName=productName;
         this.productDescription=productDescription;
         this.producer=producer;
         this.productQuantityOnStock=productQuantityOnStock;
         this.productPrice=productPrice;
     }
-    public void addProduct(String productName, String productDescription, String producer, int productQuantityOnStock, double productPrice){
-        products.add(new Product(productName,productDescription,producer,productQuantityOnStock,productPrice));
+    public void addProduct(String groupNameInProduct, String productName, String productDescription, String producer, int productQuantityOnStock, double productPrice){
+        products.add(new Product(groupNameInProduct,productName,productDescription,producer,productQuantityOnStock,productPrice));
     }
-    public void editProduct(String productName, String productDescription, String producer, int productQuantityOnStock, double productPrice){
+    public void editProduct(String groupNameInProduct, String productName, String productDescription, String producer, int productQuantityOnStock, double productPrice){
+        setGroupNameInProduct(groupNameInProduct);
         setProductName(productName);
         setProductDescription(productDescription);
         setProducer(producer);
@@ -37,20 +40,29 @@ public class Product {
         products.remove(index);
     }
 
+    /**Use when you remove group to remove all products in this group*/
+    public void remove(String groupName){
+        for(int i=0; i<products.size(); i++)
+            if(groupName.equals(products.get(i).getGroupNameInProduct()))
+                products.remove(i);
+    }
+
+    /**toString() in Product.class*/
     public String toString(){
-        return "#"+productName+
+        return "#"+groupNameInProduct+
+                "\nНазва: "+productName+
                 "\nОпис: "+productDescription+
                 "\nВиробник: "+producer+
                 "\nНа складі: "+productQuantityOnStock+
                 "\nЦіна за штуку: "+productPrice;
     }
 
-
-
-
     /**
      * getters and setter to fields
      * */
+    public String getGroupNameInProduct() {return groupNameInProduct;}
+    public void setGroupNameInProduct(String groupNameInProduct) {this.groupNameInProduct = groupNameInProduct;}
+
     public String getProductName() {return productName;}
     public void setProductName(String productName) {this.productName = productName;}
 
